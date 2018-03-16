d5bf463919be8aa3ababac577241c01c1af47bfd
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-16 14:11:02 +0000

    Guest Class and Spec writtem.

---
+++ guest.rb
@@ -0,0 +1,10 @@
+class Guest
+
+
+attr_reader :name
+
+  def initialize(input_name)
+    @name = input_name
+  end
+
+end
---
+++ specs/guest_spec.rb
@@ -0,0 +1,17 @@
+require("minitest/autorun")
+require_relative("../guest.rb")
+
+class TestGuest < MiniTest::Test
+
+  def setup
+    @guest1 = Guest.new("Mike")
+    @guest2 = Guest.new("Mariana")
+    @guest3 = Guest.new("Andrew")
+    @guest4 = Guest.new("Claudia")
+  end
+
+  def test_creation_of_guest
+    assert_equal("Mike",@guest1.name())
+  end
+
+end
